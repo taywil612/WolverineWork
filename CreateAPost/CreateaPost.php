6c05960afb7d8add7a8f368288872f0e67ea904c
Your Name <you@example.com>
2023-11-27 21:39:48 -0500

    Added reactive side navigation to home/CaP

--- CreateAPost/CreateaPost.php
+++ CreateAPost/CreateaPost.php
@@ -2,10 +2,10 @@
 session_start();
 
 // Check if logged in 
-// if (!isset($_SESSION['loggedin'])) {
-//     header("Location: ../login/login-form.php");
-//     exit();
-// }
+if (!isset($_SESSION['loggedin'])) {
+    header("Location: ../login/login-form.php");
+    exit();
+}
 
 // Connect to the database
 include('../database/connection.php');
@@ -46,30 +46,42 @@ if ($_POST) {
     <title>Submit Your Written Work</title>
     <link href='https://fonts.googleapis.com/css?family=Nunito+Sans' rel='stylesheet'>
     <link rel="shortcut icon" type="image/png" href="../img/placeholderlogo.png" />
-    <link href='../NavigationBar/nav.css' rel='stylesheet'>
+    <link href='../NavigationBar/side-nav-ss.css' rel='stylesheet'>
     <link href='CaP.css' rel='stylesheet'>
+    <style>
+        #mySidebar {
+            width: 0;
+        }
+
+        .c {
+            margin-left: 0; /* Set initial margin to 0 */
+        }
+    </style>
 </head>
 <body>
-    <div class="container">
-        <?php include("../NavigationBar/sidenav.html")?>
-        <h2>Submit your written work here!</h2>
-        <form method="POST" enctype="multipart/form-data">
-            <div class="centered-container">
-                <input type="text" name='title' placeholder="Title your work..." required>
-                <textarea rows="8" name='caption' placeholder="Add a caption..."></textarea>
-                <br>
-                <div class="upload-container">
-                <input type="file" class="custom-file-input" name="file-upload[]" id="file-upload" onchange="updateFileName(this)" accept=".pdf, .docx" required>
-                    <label for="file-upload" class="custom-file-label">
-                        <span class="custom-file-label-text">Click to browse</span>
-                        <span class="custom-file-label-text">.docx or .pdf</span>
-                        <span class="custom-file-label-text">Max 10 MB</span>
-                    </label>
-                    <div id="uploaded-files">No file selected</div>
+    <?php include('../NavigationBar/side-nav.html'); ?>
+    <div class="c">
+        <div class="content">
+            <div class="menu" onclick="openFilter()">☰</div>
+            <h2>Submit your written work here!</h2>
+            <form method="POST" enctype="multipart/form-data">
+                <div class="centered-container">
+                    <input type="text" name='title' placeholder="Title your work..." required>
+                    <textarea rows="8" name='caption' placeholder="Add a caption..."></textarea>
+                    <br>
+                    <div class="upload-container">
+                    <input type="file" class="custom-file-input" name="file-upload[]" id="file-upload" onchange="updateFileName(this)" accept=".pdf, .docx" required>
+                        <label for="file-upload" class="custom-file-label">
+                            <span class="custom-file-label-text">Click to browse</span>
+                            <span class="custom-file-label-text">.docx or .pdf</span>
+                            <span class="custom-file-label-text">Max 10 MB</span>
+                        </label>
+                        <div id="uploaded-files">No file selected</div>
+                    </div>
                 </div>
-            </div>
-            <button type="submit" class="post-button">Post</button>
-        </form>
+                <button type="submit" class="post-button">Post</button>
+            </form>
+        </div>
     </div>
 
     <script>
@@ -78,6 +90,17 @@ if ($_POST) {
             const uploadedFiles = document.getElementById("uploaded-files");
             uploadedFiles.textContent = input.files[0] ? input.files[0].name : "No file selected";
         }
-    </script>
+
+        // JS functions to open and close the sidebar
+        function openFilter() {
+            document.getElementById("mySidebar").style.width = "300px";
+            document.getElementsByClassName("c")[0].style.marginLeft = "300px"; // Adjusted margin
+        }
+
+        function closeFilter() {
+            document.getElementById("mySidebar").style.width = "0";
+            document.getElementsByClassName("c")[0].style.marginLeft = "0";
+        }
+        </script>
 </body>
 </html>
